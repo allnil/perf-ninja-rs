e7be0ce5237764d5602755ef35ddd715867b0109
allnil <nil.medvedev17@gmail.com>
2026-03-16 14:59:41 +0400

    warmup

--- labs/misc/warmup/src/lib.rs
+++ labs/misc/warmup/src/lib.rs
@@ -2,9 +2,6 @@
 mod tests;
 
 pub fn solution(arr: &[i32], n: usize) -> i32 {
-    let mut res = 0;
-    for i in 0..n {
-        res += arr[i];
-    }
-    res
+    let N = n as i32;
+    (N * (N + 1)) / 2
 }
